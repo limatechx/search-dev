c53329be186f770f480bd50ca27de63e2b991cb9
Carlos Eduardo Lima <carloseduardolima.dev@gmail.com>
2025-08-22 08:58:54 -0300

    feat: irequisicao da API e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,11 @@ import { PerfilComponent } from './pages/perfil/perfil.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+  private apiUrl = 'https://api.github.com/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUser(username: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${username}`);
+  }
+
+  getRepos(username: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${username}/repos`);
+  }
 }
+
