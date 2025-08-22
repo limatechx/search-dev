03c51a01b8c953cda3e8e86053867a48dedfb52e
Carlos Eduardo Lima <carloseduardolima.dev@gmail.com>
2025-08-22 08:55:31 -0300

    feat: implementacao lógica do perfil e home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { GithubService } from '../../github.service'; // ajuste o caminho
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  username: string = '';
+  errorMessage: string = '';
 
+  constructor(private router: Router, private githubService: GithubService) { }
+
+  buscar() {
+    const nome = this.username.trim();
+
+    this.githubService.getUser(nome).subscribe({
+      next: () => {
+        this.errorMessage = '';
+        this.router.navigate(['/perfil', nome]);
+      },
+      error: () => {
+        this.errorMessage = 'Usuário não encontrado!';
+        this.username = '';
+      }
+    });
+  }
+
+  limparErro() {
+    this.errorMessage = '';
+  }
 }
+
+
--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GithubService } from '../../github.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.scss']
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
+  user: any;
+  repos: any[] = [];
+  errorMessage: string = '';
+  username: string = '';
 
+  constructor(private route: ActivatedRoute, private githubService: GithubService, private router: Router) { }
+
+  ngOnInit(): void {
+    // Observa mudanças no parâmetro "username"
+    this.route.paramMap.subscribe(params => {
+      this.username = params.get('username') || '';
+      this.buscarUsuario();
+    });
+  }
+
+  buscarUsuario() {
+  this.errorMessage = ''; // <--- limpa a mensagem de erro antiga
+
+  if (!this.username) return;
+
+  this.githubService.getUser(this.username).subscribe({
+    next: data => {
+      this.user = data;
+      this.errorMessage = ''; 
+    },
+    error: () => {
+      this.user = null;
+      this.repos = [];
+      this.errorMessage = 'Usuário não encontrado!'; // 
+    }
+  });
+
+  this.githubService.getRepos(this.username).subscribe({
+    next: data => {
+      this.repos = data.sort((a: any, b: any) => b.stargazers_count - a.stargazers_count);
+    },
+    error: () => this.repos = []
+  });
 }
+
+  voltar() {
+    this.router.navigate(['/home']);
+  }
+}
+
+
